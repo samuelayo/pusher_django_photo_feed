22c68a39397934d4bed6430fc9bb5986e4978854
samuel ogundipe <ayoogundipe2005@gmail.com>
2017-05-22 23:08:05 +0100

    changed to private channel

--- feed/views.py
+++ feed/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from .forms import *
 from pusher import Pusher
+import json
+
 
 #instantiate pusher
 pusher = Pusher(app_id=u'XXX_APP_ID', key=u'XXX_APP_KEY', secret=u'XXX_APP_SECRET', cluster=u'XXX_APP_CLUSTER')
@@ -13,6 +15,16 @@ def index(request):
     # return the index.html template, passing in all the feeds
     return render(request, 'index.html', {'all_documents': all_documents})
 
+def pusher_authentication(request):
+    channel = request.GET.get('channel_name', None)
+    socket_id = request.GET.get('socket_id', None)
+    auth = pusher.authenticate(
+      channel = channel,
+      socket_id = socket_id
+    )
+ 
+    return JsonResponse(json.dumps(auth), safe=False)
+
 #function that triggers the pusher request
 def push_feed(request):
     # check if the method is post
@@ -22,7 +34,7 @@ def push_feed(request):
         if form.is_valid():
             f = form.save()
             # trigger a pusher request after saving the new feed element 
-            pusher.trigger(u'a_channel', u'an_event', {u'description': f.description, u'document': f.document.url})
+            pusher.trigger(u'a-test_channel', u'an_event', {u'description': f.description, u'document': f.document.url})
             return HttpResponse('ok')
         else:
             # return a form not valid error
@@ -30,3 +42,5 @@ def push_feed(request):
     else:
        # return error, type isnt post
        return HttpResponse('error, please try again')
+
+
--- photofeed/urls.py
+++ photofeed/urls.py
@@ -20,5 +20,6 @@ from feed.views import *
 urlpatterns = [
     url(r'^$', index),
     url(r'^push_feed$', push_feed),
+    url(r'^pusher_authentication', pusher_authentication),
     url(r'^admin/', admin.site.urls),
 ]
